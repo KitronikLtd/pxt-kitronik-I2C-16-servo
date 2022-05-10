b72d38066c7a8b3365d58ab3a9aaf1b645f6f712
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-05-09 18:18:10 -0700

    fix tests

--- tests.ts
+++ tests.ts
@@ -2,26 +2,26 @@
 // servo 8 to 180 degrees
 // servo 16 to 0 degrees
 input.onButtonPressed(Button.A, () => {
-    kitronik.servoWrite(kitronik.Servos.Servo1, 90);
-	kitronik.servoWrite(kitronik.Servos.Servo8, 180);
-	kitronik.servoWrite(kitronik.Servos.Servo16, 0);
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 90);
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, 180);
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo16, 0);
 })
 
 // servo 1 to 180 degrees
 // servo 8 to 0 degrees
 // servo 16 to 90 degrees
 input.onButtonPressed(Button.B, () => {
-	kitronik.servoWrite(kitronik.Servos.Servo1, 180);
-    kitronik.servoWrite(kitronik.Servos.Servo8, 0);
-	kitronik.servoWrite(kitronik.Servos.Servo16, 90);
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 180);
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, 0);
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo16, 90);
 })
 
 // servo 1 to 0 degrees
 // servo 8 to 90 degrees
 // servo 16 to 180 degrees
 input.onButtonPressed(Button.AB, () => {
-	kitronik.servoWrite(kitronik.Servos.Servo1, 0);
-	kitronik.servoWrite(kitronik.Servos.Servo8, 90);
-   kitronik.servoWrite(kitronik.Servos.Servo16, 180);
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 0);
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo8, 90);
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo16, 180);
 })
 
